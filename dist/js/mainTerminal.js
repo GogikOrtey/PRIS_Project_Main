6ac937eccb0b99209130a3a8fce74ffcd3c76f5f
Gogik <gog.ortey@yandex.ru>
2024-01-08 22:48:51 +1000

    9 - 8.11.2023 - Начал писать обработку событий на JS

--- dist/js/mainTerminal.js
+++ dist/js/mainTerminal.js
@@ -96,6 +96,7 @@ h_NoControl = 0
 // 2 - На 1-2 недели
 // 3 - До месяца
 
+let isStart = false;
 
 // Ждём, пока DOM-модель полностью загрузится
 document.addEventListener('DOMContentLoaded', function() { 
@@ -114,36 +115,20 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log("123");
         document.getElementById('block-a').style.display = 'grid';
         document.getElementsByClassName('descr-qu-1-grad')[0].style.display = 'none';
+        isStart = true;
     });
 
-
-
-    // Создайте объект для хранения информации о кнопках и блоках
-    let buttonBlockMap = {
-        'block-a': {
-            'answ-block-1': 'block-a-1',
-            'answ-block-2': 'block-a-2'
-        },
-        // Добавьте другие блоки по мере необходимости
-    };
-
-    // Функция для отображения определенного блока
-    function showBlock(blockId) {
-        //hideAllBlocks();
-        document.getElementById(blockId).style.display = 'grid';
-        blockState[blockId] = true;
-    }
-
-    // Добавьте обработчики событий для кнопок
+    // Добавляю событие: При нажатии на любую кнопку
     document.querySelectorAll('.butt-answ').forEach(button => {
         button.addEventListener('click', function() {
-            // Сначала уберите стиль :active со всех кнопок в этом блоке
+            CheckAllBlocks(); // Вызываю обновление всех блоков
+
+            // Добавляю стиль "Нажатой" кнопки к той, которую нажал пользователь
             this.parentElement.querySelectorAll('.butt-answ').forEach(otherButton => {
+                // Удаляю стиль "Нажатой" кнопки у всех в этом блоке
                 otherButton.classList.remove('active');
             });
-
-            // Затем добавьте стиль :active к нажатой кнопке
-            this.classList.add('active');
+            this.classList.add('active'); // Добавляю этот стиль к нажатой
         });
     });
 
@@ -151,8 +136,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.querySelector('#block-a .answ-block-1').addEventListener('click', function() {
         a_InHome = 1
-    });
-    
+    });    
     document.querySelector('#block-a .answ-block-2').addEventListener('click', function() {
         a_InHome = 2
     });
@@ -161,26 +145,162 @@ document.addEventListener('DOMContentLoaded', function() {
         a_2_AVGTempInRegion = 1
         // Ввод из поля ввода
         //a_2_input_AVGTempInRegion = ...
-    });
-    
-    document.querySelector('#block-a-1 .answ-block-2').addEventListener('click', function() {
+    });    
+
+    document.querySelector('#block-a-2 .answ-block-1').addEventListener('click', function() {
         //a_InHome = 1
         a_1_MinTempInHome = 1
         // Ввод из поля ввода
         //a_1_input_MinTempInHome = ...
     });
-    
 
-    // // Добавьте обработчики событий для кнопок
-    // document.querySelectorAll('button').forEach(button => {
-    //     button.addEventListener('click', function() {
-    //         let parentBlockId = this.parentElement.id;
-    //         let buttonClass = this.className;
-    //         if (buttonBlockMap[parentBlockId] && buttonBlockMap[parentBlockId][buttonClass]) {
-    //             showBlock(buttonBlockMap[parentBlockId][buttonClass]);
-    //         }
-    //     });
-    // });
+    document.querySelector('#block-a-2-1 .answ-block-1-s').addEventListener('click', function() {
+        a_2_1_AVGHum = 1
+        // Ввод из поля ввода
+        //a_2_1_input_AVGHum = ...
+    });    
+    document.querySelector('#block-a-2-1 .answ-block-2-s').addEventListener('click', function() {
+        a_2_1_AVGHum = 2
+    });    
+    document.querySelector('#block-a-2-1 .answ-block-3-s').addEventListener('click', function() {
+        a_2_1_AVGHum = 3
+    });    
+    document.querySelector('#block-a-2-1 .answ-block-4-s').addEventListener('click', function() {
+        a_2_1_AVGHum = 4
+    });    
 
+    document.querySelector('#block-b .answ-block-1').addEventListener('click', function() {
+        b_OncePlant = 1
+    });    
+    document.querySelector('#block-b .answ-block-2').addEventListener('click', function() {
+        b_OncePlant = 2
+    });
+
+    document.querySelector('#block-c .answ-block-1').addEventListener('click', function() {
+        c_AFlowers = 1
+    });    
+    document.querySelector('#block-c .answ-block-2').addEventListener('click', function() {
+        c_AFlowers = 2
+    });
+    document.querySelector('#block-c .answ-block-3').addEventListener('click', function() {
+        c_AFlowers = 3
+    });
+
+    //c-3
+
+    document.querySelector('#block-d .answ-block-1').addEventListener('click', function() {
+        d_IsPlod = 1
+    });    
+    document.querySelector('#block-d .answ-block-2').addEventListener('click', function() {
+        d_IsPlod = 2
+    });
+    document.querySelector('#block-d .answ-block-3').addEventListener('click', function() {
+        d_IsPlod = 3
+    });
+
+    document.querySelector('#block-e .answ-block-1').addEventListener('click', function() {
+        e_StandOnWindow = 1
+    });    
+    document.querySelector('#block-e .answ-block-2').addEventListener('click', function() {
+        e_StandOnWindow = 2
+    });
+
+    document.querySelector('#block-e-1 .answ-block-1').addEventListener('click', function() {
+        e_1_ASunLight = 1
+    });    
+    document.querySelector('#block-e-1 .answ-block-2').addEventListener('click', function() {
+        e_1_ASunLight = 2
+    });
+    document.querySelector('#block-e-1 .answ-block-3').addEventListener('click', function() {
+        e_1_ASunLight = 3
+    });
+
+    document.querySelector('#block-f .answ-block-1').addEventListener('click', function() {
+        f_GenerateAOxugen = 1
+    });    
+    document.querySelector('#block-f .answ-block-2').addEventListener('click', function() {
+        f_GenerateAOxugen = 2
+    });
+    document.querySelector('#block-f .answ-block-3').addEventListener('click', function() {
+        f_GenerateAOxugen = 3
+    });
+
+    document.querySelector('#block-g .answ-block-1').addEventListener('click', function() {
+        g_AFreeProstr = 1
+    });    
+    document.querySelector('#block-g .answ-block-2').addEventListener('click', function() {
+        g_AFreeProstr = 2
+    });
+    document.querySelector('#block-g .answ-block-3').addEventListener('click', function() {
+        g_AFreeProstr = 3
+    });
+
+    document.querySelector('#block-h .answ-block-1').addEventListener('click', function() {
+        h_NoControl = 1
+    });    
+    document.querySelector('#block-h .answ-block-2').addEventListener('click', function() {
+        h_NoControl = 2
+    });
+    document.querySelector('#block-h .answ-block-3').addEventListener('click', function() {
+        h_NoControl = 3
+    });
 
 });
+
+// Перераспределение блоков
+// Процедура вызывается после нажатия на кнопку в любом блоке
+function CheckAllBlocks(){
+    if(isStart == true) {
+        console.log("Update all blocks")
+
+        if (a_InHome == 2) {
+            document.getElementById('block-a-1').style.display = 'grid';
+            document.getElementById('block-a-2').style.display = 'none';
+            //document.getElementsByClassName('descr-qu-1-grad')[0].style.display = 'none';
+        } else if(a_InHome == 1) {
+            document.getElementById('block-a-1').style.display = 'none';
+            document.getElementById('block-a-2').style.display = 'grid';
+        }
+
+        //if()
+    }
+}
+
+function debugPrint(){
+    console.log(`a_InHome: ${a_InHome}`);
+    if (a_InHome == 1) {
+        console.log(`a_1_MinTempInHome: ${a_1_MinTempInHome}`);
+        if (a_1_MinTempInHome == 1) {
+            console.log(`a_1_input_MinTempInHome: ${a_1_input_MinTempInHome}`);
+        }
+    } else {
+        console.log(`a_2_AVGTempInRegion: ${a_2_AVGTempInRegion}`);
+        if (a_2_AVGTempInRegion == 1) {
+            console.log(`a_2_input_AVGTempInRegion: ${a_2_input_AVGTempInRegion}`);
+        }
+        console.log(`a_2_1_AVGHum: ${a_2_1_AVGHum}`);
+        if (a_2_1_AVGHum == 1) {
+            console.log(`a_2_1_input_AVGHum: ${a_2_1_input_AVGHum}`);
+        }
+    }
+    
+    console.log(`b_OncePlant: ${b_OncePlant}`);
+    
+    console.log(`c_AFlowers: ${c_AFlowers}`);
+    if (c_AFlowers == 3) {
+        console.log(`c_3_SelectAColor: ${c_3_SelectAColor}`);
+    }
+    
+    console.log(`d_IsPlod: ${d_IsPlod}`);
+    
+    console.log(`e_StandOnWindow: ${e_StandOnWindow}`);
+    if (e_StandOnWindow == 1) {
+        console.log(`e_1_ASunLight: ${e_1_ASunLight}`);
+    }
+    
+    console.log(`f_GenerateAOxugen: ${f_GenerateAOxugen}`);
+    
+    console.log(`g_AFreeProstr: ${g_AFreeProstr}`);
+    
+    console.log(`h_NoControl: ${h_NoControl}`);    
+}
